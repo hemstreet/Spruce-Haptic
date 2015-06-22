7155e388c403ed1b994f4f5b389a9278fd814cc8
Hemstreet <jonhemstreet@gmail.com>
2015-06-22 11:56:19 -0600

    progress

--- wristband.ino
+++ wristband.ino
@@ -1,9 +1,5 @@
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
-#include "WebServer.h"
-
-#define PREFIX ""
-WebServer webserver(PREFIX, 80);
 
 // If using software SPI (the default case):
 #define OLED_MOSI   D0
@@ -19,31 +15,15 @@ Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 int isVibrating = 0;
 
-void helloCmd(WebServer &server, WebServer::ConnectionType type, char *, bool)
-{
-  /* this line sends the standard "we're all OK" headers back to the
-     browser */
-  server.httpSuccess();
-
-  /* if we're handling a GET or POST, we can output our data here.
-     For a HEAD request, we just stop after outputting headers. */
-  if (type != WebServer::HEAD)
-  {
-    /* this defines some HTML text in read-only memory aka PROGMEM.
-     * This is needed to avoid having the string copied to our limited
-     * amount of RAM. */
-    P(helloMsg) = "<h1>Hello, World!</h1>";
-
-    /* this is a special form of print that outputs from PROGMEM */
-    server.printP(helloMsg);
-  }
-}
+void testScreen(String command);
 
 void setup()   {                
     Serial.begin(9600);
 
+    //Spark.function("test", testScreen);
+
   pinMode(D7, OUTPUT);
-  
+
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC);
   
@@ -60,17 +40,27 @@ void setup()   {
   display.display();
   delay(2000);
 
-   /* setup our default command that will be run when the user accesses
-     * the root page on the server */
-    webserver.setDefaultCommand(&helloCmd);
-
-    /* run the same command if you try to load /index.html, a common
-     * default page name */
-    webserver.addCommand("index.html", &helloCmd);
-
-    /* start the webserver */
-    webserver.begin();
   
+}
+
+void testScreen(String command)
+{
+
+  //if(command == "testing")
+  //{
+
+   display.clearDisplay();
+
+    display.setTextSize(1.5);
+    display.setTextColor(WHITE);
+    display.println("Test Screen");
+    display.println("Type: Testing");
+    display.println("Size: 1 1/2");
+    display.println("Qty: 5");
+    display.display();
+
+  //}
+
 }
 
 void loop() {
@@ -79,11 +69,5 @@ void loop() {
 //    digitalWrite(D7, (isVibrating) ? HIGH : LOW);
 //    isVibrating = !isVibrating;
 //    delay(2500);
-
-     char buff[64];
-      int len = 64;
-
-      /* process incoming connections one at a time forever */
-      webserver.processConnection(buff, &len);
     
 }
